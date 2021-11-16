fcaf538f3fcd2d448a250af72afb4bd727684fd5
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-11-16 15:00:22 -0300

    button on submit

--- src/pages/implementation-references/react-hook-form.tsx
+++ src/pages/implementation-references/react-hook-form.tsx
@@ -2,23 +2,33 @@ import { NextPage } from 'next';
 import { useEffect } from 'react';
 import { SubmitHandler, useForm, FormProvider } from 'react-hook-form';
 import { TextInput } from '@/components/forms/text-input';
+import { Button } from 'antd';
 interface ISampleForm {
-  value: string;
+  usuario: string;
+  projeto: string;
 }
 
 const ReactHookFormPage: NextPage = () => {
   const RHForm = useForm();
-  const onSubmit: SubmitHandler<ISampleForm> = data => console.log(data);
+
+  const onSubmit: SubmitHandler<ISampleForm> = data =>
+    console.log('Submit', data);
+
   const formChanges = RHForm.watch();
-  useEffect(() => console.log(formChanges), [formChanges]);
+  useEffect(() => console.log('formChanges', formChanges), [formChanges]);
+
   return (
     <FormProvider {...RHForm}>
       <form
         onSubmit={RHForm.handleSubmit(onSubmit)}
         className="w-full flex flex-col space-y-4 justify-center items-center"
       >
-        <TextInput name="value" placeholder="" />
+        <TextInput name="usuario" placeholder="usuario" />
+        <TextInput name="projeto" placeholder="projeto" />
         <h1>{formChanges.value}</h1>
+        <Button type="primary" htmlType="submit">
+          Enviar
+        </Button>
       </form>
     </FormProvider>
   );
