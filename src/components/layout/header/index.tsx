1c4eebb659dcd77505a6556f0686b68b932d55e1
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-11-24 16:29:22 -0300

    fix header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -6,12 +6,12 @@ export const Header = () => {
   return (
     <header
       className={
-        'w-full h-16 z-50 sticky flex flex-col items-center justify-center content-center bg-gray-100 shadow-md'
+        'w-full h-16 z-50 relative flex flex-col items-center justify-center content-center bg-gray-100 shadow-md'
       }
     >
       <nav
         className={
-          'w-full px-2 h-full max-w-7xl flex space-x-4 content-center items-center'
+          'w-full px-2 h-16 max-w-7xl flex space-x-4 content-center items-center'
         }
       >
         <HeaderTitle name="Sample Frontend Title" />
